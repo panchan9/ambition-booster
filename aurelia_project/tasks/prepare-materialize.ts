d3e33b56ab33856e42fd3ab73f72563ec775f2aa
pampers <pepperoni9@gmail.com>
2017-10-15 12:50:15 +0900

    make Materialize available with aurelia-materialize-bridge

---
+++ aurelia_project/tasks/prepare-materialize.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+import * as merge from 'merge-stream';
+import * as gulp from 'gulp';
+
+
+export default function prepareMaterialize() {
+
+  let source = 'node_modules/materialize-css/dist';
+  let sourceStyle = path.join(source, 'css');
+  let sourceFonts = path.join(source, 'fonts/roboto');
+
+  let taskCss = gulp.src(
+    path.join(sourceStyle, 'materialize.min.css'),
+    { base: sourceStyle }
+  )
+  .pipe(gulp.dest('styles'));
+
+  let taskFonts = gulp.src(
+    path.join(sourceFonts, '*'),
+    { base: sourceFonts }
+  )
+  .pipe(gulp.dest('fonts/roboto'));
+
+  return merge(taskCss, taskFonts);
+}
